ff7fefddefbab8496fbe070849376958c34f8700
yyay0001 <yayehirad.melsew1@monash.edu>
2022-04-07 11:56:06 +1000

    fix intiger

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
+    num0, num1 = int(num0), int(num1)
     if operator == '+':
         return num0 + num1
     elif operator == '-':
@@ -7,4 +8,4 @@ def compute(expression):
     else:
         print('unknown operator!')
         return None
-// to use unpacking branch
+#// to use unpacking branch
